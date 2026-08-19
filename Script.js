7db222c91e1ddd7c40065b157bedbde9f7bec119
rafaelademelo-max <rafaela.de.melo@escola.pr.gov.br>
2026-08-19 19:46:35 -0300

    segunda parte

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
+const dataAtual = new Date();
 
+const dataFormatada = dataAtual.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+        month: "2-digit",
+            year: "numeric"
+            });
+
+            document.getElementById("data").textContent = dataFormatada;
